62d51b77104dc1bc6775c6b9f3cb5d7f02863101
Nechay Vova <nechayvova2005@gmail.com>
2025-10-08 13:12:18 +0300

    Fix all minor misstakes 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,8 @@ export const App: React.FC = () => {
 
   useEffect(() => {
     getTodosList();
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     setTimeout(() => {
